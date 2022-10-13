921236e515612f77a78fff84b64565ef4be39a3b
cland29 <carl.lee.landskron@gmail.com>
2022-10-12 20:30:31 -0400

    Add Newton Method Computation

--- NewtonMethod.java
+++ NewtonMethod.java
@@ -1,8 +1,20 @@
-
-
+import java.util.function.DoubleFunction;
+import java.util.function.DoubleSupplier;
+import java.util.function.Function;
 
 class NewtonMethod{
-    public NewtonMethod(){
-        
+    DoubleFunction<Double> function, derivative;
+
+    public NewtonMethod(DoubleFunction<Double> function, DoubleFunction<Double> derivative){
+        this.function = function;
+        this.derivative = derivative;
+    }
+
+    public double computeNthIteration(int n, int x0Estimate){
+        if (n <= 0){
+            return x0Estimate;
+        }
+        double priorTerm = computeNthIteration(n - 1, x0Estimate);
+        return priorTerm + function.apply(priorTerm) / derivative.apply(priorTerm);
     }
 }
