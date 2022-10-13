7ca80f49b68d2381fcf3057f9467ece7c82ec380
cland29 <carl.lee.landskron@gmail.com>
2022-10-12 21:32:12 -0400

    Add Secant Methods

--- SecantMethod.java
+++ SecantMethod.java
@@ -1,6 +1,23 @@
+import java.util.function.DoubleFunction;
 
 class SecantMethod{
-    public SecantMethod(){
-        
+
+    DoubleFunction<Double> function;
+
+    public SecantMethod(DoubleFunction<Double> function){
+        this.function = function;
+    }
+
+    public double computeNthIteration(int n, double x0Estimate, double x1Estimate){
+        if (n <= 0){
+            return x0Estimate;
+        }
+        if (n == 1){
+            return x1Estimate;
+        }
+
+        double doublePriorTerm = computeNthIteration(n - 2, x0Estimate, x1Estimate);
+        double priorTerm = computeNthIteration(n - 1, x0Estimate, x1Estimate);
+        return priorTerm - function.apply(priorTerm) * ((priorTerm - doublePriorTerm)/(function.apply(priorTerm) - function.apply(doublePriorTerm)));
     }
 }
